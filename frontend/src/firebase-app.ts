70d2f7e9f65b431c880fb4cda58742251e96ac48
rtokkie <natto.and.eggs@gmail.com>
2022-11-29 12:16:58 +0900

    a

---
+++ frontend/src/firebase-app.ts
@@ -0,0 +1,11 @@
+import { initializeApp } from 'firebase/app';
+
+initializeApp({
+  apiKey: 'AIzaSyCBe1aMcQzZEsASF-588VaYuub90uIzNPU',
+  authDomain: 'fir-auth-3e082.firebaseapp.com',
+  projectId: 'fir-auth-3e082',
+  storageBucket: 'fir-auth-3e082.appspot.com',
+  messagingSenderId: '749021592402',
+  appId: '1:749021592402:web:ccb2c6f713ed5b0808d88b',
+  measurementId: 'G-1C7DJPNCW7',
+});
